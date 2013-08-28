57959a3d3fdb8e0200f62bd2bc949c19f515956a
Ilter Cengiz <me@iltercengiz.info>
2013-08-28 16:30:12 +0300

    Initial Commit

---
+++ ICViewPager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ICViewPager
+//
+//  Created by Ilter Cengiz on 28/08/2013.
+//  Copyright (c) 2013 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
