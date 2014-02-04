d6cc5c1315ee7cb24a19ea8ba1622dd681b03911
FrankWu100 <frankwu100@gmail.com>
2014-02-04 21:26:28 +0800

    Update & Fix issue
    Update Storyboard to Xcode 5 & iOS7
    
    Fix if iOS 7 Navigation Bar type is Translucent, don’t need to set
    topLayoutGuide.

--- ICViewPager/ICViewPager/ViewPagerController.m
+++ ICViewPager/ICViewPager/ViewPagerController.m
@@ -203,7 +203,7 @@
 - (void)layoutSubviews {
     
     CGFloat topLayoutGuide = 0.0;
-    if (IOS_VERSION_7) {
+    if (IOS_VERSION_7 && self.navigationController.navigationBar.translucent) {
         topLayoutGuide = 20.0;
         if (self.navigationController && !self.navigationController.navigationBarHidden) {
             topLayoutGuide += self.navigationController.navigationBar.frame.size.height;
