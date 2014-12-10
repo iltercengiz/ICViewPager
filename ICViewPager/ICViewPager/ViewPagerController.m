af5d423fdf46d8d5d50682be51b87783db4b6807
Oliver Bridgman <oliver@3months.com>
2014-12-10 16:41:34 +1300

    Remove the borders

--- ICViewPager/ICViewPager/ViewPagerController.m
+++ ICViewPager/ICViewPager/ViewPagerController.m
@@ -84,22 +84,6 @@
     
     UIBezierPath *bezierPath;
     
-    // Draw top line
-    bezierPath = [UIBezierPath bezierPath];
-    [bezierPath moveToPoint:CGPointMake(0.0, 0.0)];
-    [bezierPath addLineToPoint:CGPointMake(CGRectGetWidth(rect), 0.0)];
-    [[UIColor colorWithWhite:197.0/255.0 alpha:0.75] setStroke];
-    [bezierPath setLineWidth:1.0];
-    [bezierPath stroke];
-    
-    // Draw bottom line
-    bezierPath = [UIBezierPath bezierPath];
-    [bezierPath moveToPoint:CGPointMake(0.0, CGRectGetHeight(rect))];
-    [bezierPath addLineToPoint:CGPointMake(CGRectGetWidth(rect), CGRectGetHeight(rect))];
-    [[UIColor colorWithWhite:197.0/255.0 alpha:0.75] setStroke];
-    [bezierPath setLineWidth:1.0];
-    [bezierPath stroke];
-    
     // Draw an indicator line if tab is selected
     if (self.selected) {
         
