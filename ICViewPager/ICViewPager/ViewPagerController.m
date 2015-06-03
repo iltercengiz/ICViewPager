4f54389786cb90761cd30ecb063bc1f37789750b
taijizhu <taijizhu@users.noreply.github.com>
2015-06-03 20:41:15 +0800

    restore previous code

--- ICViewPager/ICViewPager/ViewPagerController.m
+++ ICViewPager/ICViewPager/ViewPagerController.m
@@ -214,12 +214,12 @@
 - (void)layoutSubviews {
     
     CGFloat topLayoutGuide = 0.0;
-//    if (IOS_VERSION_7) {
-//        topLayoutGuide = 20.0;
-//        if (self.navigationController && !self.navigationController.navigationBarHidden) {
-//            topLayoutGuide += self.navigationController.navigationBar.frame.size.height;
-//        }
-//    }
+    if (IOS_VERSION_7) {
+        topLayoutGuide = 20.0;
+        if (self.navigationController && !self.navigationController.navigationBarHidden) {
+            topLayoutGuide += self.navigationController.navigationBar.frame.size.height;
+        }
+    }
     
     CGRect frame = self.tabsView.frame;
     frame.origin.x = 0.0;
