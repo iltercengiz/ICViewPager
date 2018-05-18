0800da6431507302e5a25c7913b002e20e77c8f0
Ilter Cengiz <iltercengiz@yahoo.com>
2018-05-18 18:17:39 +0400

    Clean up
    - Project version updated
    - Updated project setup
    - Swift 4.1
    - Legacy source moved into a separate group
    - Created empty screen for control purposes

---
+++ ICViewPager/Application/ApplicationDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  ApplicationDelegate.swift
+//  ICViewPager
+//
+//  Created by Ilter Cengiz on 18/5/18.
+//  Copyright © 2018 Ilter Cengiz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class ApplicationDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = EmptyViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
---
+++ ICViewPager/Scenes/Empty/EmptyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyViewController.swift
+//  ICViewPager
+//
+//  Created by Ilter Cengiz on 18/5/18.
+//  Copyright © 2018 Ilter Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class EmptyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.7105011344, green: 0.9193516374, blue: 1, alpha: 1)
+    }
+}
