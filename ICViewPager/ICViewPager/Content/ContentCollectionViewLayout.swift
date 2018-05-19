4be4ad7c0cf2804fd1d0e3e0f957b31c728e2a8d
Ilter Cengiz <iltercengiz@yahoo.com>
2018-05-19 21:06:41 +0400

    Implement paging layout

--- ICViewPager/ICViewPager/Content/ContentCollectionViewLayout.swift
+++ ICViewPager/ICViewPager/Content/ContentCollectionViewLayout.swift
@@ -12,21 +12,42 @@ final class ContentCollectionViewLayout: UICollectionViewFlowLayout {
     
     override init() {
         super.init()
-        setUpLayout()
+        scrollDirection = .horizontal
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setUpLayout()
-    }
-}
-
-private extension ContentCollectionViewLayout {
-    
-    func setUpLayout() {
         scrollDirection = .horizontal
-        sectionInset = .zero
-        minimumInteritemSpacing = 0.0
-        minimumLineSpacing = 0.0
+    }
+    
+    override func prepare() {
+        super.prepare()
+        guard let collectionView = collectionView else { return }
+        collectionView.decelerationRate = UIScrollViewDecelerationRateFast
+        collectionView.isPagingEnabled = false
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
+        guard let collectionView = collectionView else { return proposedContentOffset }
+        let proposedContentOffsetCenterX = proposedContentOffset.x + collectionView.bounds.width / 2.0
+        let proposedRect = CGRect(origin: CGPoint(x: proposedContentOffset.x, y: 0.0),
+                                  size: collectionView.bounds.size)
+        let layoutAttributes = layoutAttributesForElements(in: proposedRect)?.sorted(by: {
+            // Returning `true` means $0, $1 order, whereas `false` means $1, $0 order.
+            let attr1Diff = fabs($0.center.x - proposedContentOffsetCenterX)
+            let attr2Diff = fabs($1.center.x - proposedContentOffsetCenterX)
+            return ((attr1Diff == attr2Diff && $0.indexPath.item == 0) || attr1Diff < attr2Diff)
+        })
+        guard let candidateAttributes = layoutAttributes?.first else { return proposedContentOffset }
+        // Note: There's a rounding problem on iPhone+ models for x values.
+        // It's advised to use NSInteger and check if it is smaller than zero and if so set it to zero.
+        var x = candidateAttributes.center.x - collectionView.bounds.width / 2.0
+        if x < 0 { x = 0 }
+        return CGPoint(x: x, y: proposedContentOffset.y)
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint,
+                                      withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        return targetContentOffset(forProposedContentOffset: proposedContentOffset)
     }
 }
